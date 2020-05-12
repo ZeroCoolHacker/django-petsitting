a7b9d6d2bb0d56e5cd7e8c90eb2ee91edc012e46
Shahzaib Akash <shahzaibakash@gmail.com>
2020-05-12 05:29:58 +0500

    add contact page add hero partial

--- core/urls.py
+++ core/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from .views import home
+from .views import (
+    home,
+    contact,
+)
 
 
 app_name = 'core'
@@ -9,4 +12,5 @@ app_name = 'core'
 
 urlpatterns = [
     path('', home, name='home'),
+    path('contact/', contact, name='contact'),
 ]
